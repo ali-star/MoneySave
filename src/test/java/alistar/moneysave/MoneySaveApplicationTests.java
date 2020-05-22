0ce302ab451c5642a7cd3f4f45f0ae01ce47dd88
ali-star <obanstar1@gmail.com>
2020-05-22 16:35:09 +0430

    Added AES encryption.

--- src/test/java/alistar/moneysave/MoneySaveApplicationTests.java
+++ src/test/java/alistar/moneysave/MoneySaveApplicationTests.java
@@ -1,5 +1,6 @@
 package alistar.moneysave;
 
+import alistar.moneysave.utils.AES;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import org.junit.jupiter.api.Test;
@@ -11,7 +12,12 @@ class MoneySaveApplicationTests {
 	@Test
 	void contextLoads() {
 		BearerToken token = new BearerToken(getJwt());
+		String key = "altbjq*&w12%^03altbjq*&w12%^03alt";
+		String encryptedToken = AES.encrypt(getJwt(), key);
+		String decryptedToken = AES.decrypt(encryptedToken, key);
 		System.out.println(getJwt());
+		System.out.println("encrypted token: " + encryptedToken);
+		System.out.println("decrypted token: " + decryptedToken);
 	}
 
 	private static String getJwt() {
